39eec90801c26f03ee6de6b654197b8121cb2c76
Jonatas Oliveira <dev.jonatas.oliveira@gmail.com>
2022-05-13 20:50:58 -0300

    Adicionando valores a respeito da formação

---
+++ src/values/formation.ts
@@ -0,0 +1,13 @@
+interface Formation {
+  college: string;
+  course: string;
+  period: string;
+}
+
+const formation: {
+  "pt-br": Array<Formation>;
+  en: Array<Formation>;
+} = {
+  "pt-br": [],
+  en: [],
+};
